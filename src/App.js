a9c3aa41fb004e3e9d179d1affffe5d066a2193a
Ritesh <blitzritz@gmail.com>
2021-10-15 18:51:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [pNumber, setNumber] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="shop-cart">
+        <div class="shop-cart-title">Tomato</div>
+        <div class="shop-cart-desc">Fresh as the morning breeze!</div>
+        <div class="shop-cart-slider">
+          <img
+            alt="tomato"
+            style={{ width: "100%", borderRadius: "15px" }}
+            src="http://cdn.shopify.com/s/files/1/0047/9730/0847/products/nurserylive-seeds-tomato-round-desi-vegetable-seeds-16969384296588.jpg?v=1601351553"
+          />
+        </div>
+        <div class="deets">
+          <div>
+            <label for="phone">Phone:</label>
+            <input
+              type="text"
+              id="phone"
+              onChange={(val) => {
+                setNumber(val);
+              }}
+            />
+          </div>
+        </div>
+        <div class="shop-cart-cta">
+          <div class="shop-cart-cta-price">1kg - ₹500</div>
+          <button class="btn">
+            Pay Via Simpl<span class="bg"></span>
+          </button>
+        </div>
+      </div>
+      <div class="bg"></div>
+    </>
   );
 }
 
